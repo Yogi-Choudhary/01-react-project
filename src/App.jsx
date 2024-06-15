dbe1ea36c99ae2fa741118b879a1e84672952fd5
Yogi-Choudhary <yogichoudhary9715@gmail.com>
2024-06-15 11:53:18 +0530

    component composition the special children prop

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { CORE_CONCEPTS } from './data.js';
 import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept.jsx';
+import TabButton from './components/TabButton.jsx';
+
 function App() {
   return (
     <div>
@@ -19,7 +21,15 @@ function App() {
             <CoreConcept {...CORE_CONCEPTS[3]}/>
           </ul>  
         </section>
-        
+        <section id='examples'>
+          <h2>Example</h2>
+          <menu>
+            <TabButton>Components</TabButton>
+            <TabButton>JSX</TabButton>
+            <TabButton>Props</TabButton>
+            <TabButton>State</TabButton>
+          </menu>
+        </section>
       </main>
     </div>
   );
