9206fb52c8060430eb9fd456f9bdab49289cbd58
Yogi-Choudhary <yogichoudhary9715@gmail.com>
2024-06-15 11:07:25 +0530

    Alternative props syntaxes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import reactImg from './assets/react-core-concepts.png';
+import componentsImg from './assets/components.png';
+import { CORE_CONCEPTS } from './data.js';
 
 const reactDescriptions = ['Fundamental', 'Crucial','Core'];
 
@@ -21,12 +23,35 @@ function Header() {
   );
 }
 
+function CoreConcept({image, title, description }) {
+  return (
+    <li>
+      <img src={image} alt={title}/>
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  );
+}
+
 function App() {
   return (
     <div>
       <Header/>
       <main>
-        <h2>Time to get started!</h2>
+        <section id='core-concepts'>
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept
+              title = {CORE_CONCEPTS[0].title}
+              description = {CORE_CONCEPTS[0].description}
+              image = {CORE_CONCEPTS[0].image} 
+            />
+            <CoreConcept {...CORE_CONCEPTS[1]}/>
+            <CoreConcept {...CORE_CONCEPTS[2]}/>
+            <CoreConcept {...CORE_CONCEPTS[3]}/>
+          </ul>  
+        </section>
+        
       </main>
     </div>
   );
