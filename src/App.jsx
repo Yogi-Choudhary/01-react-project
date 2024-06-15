aa74617d3a9ca7373838f7e21f9b30fe251e3531
Yogi-Choudhary <yogichoudhary9715@gmail.com>
2024-06-15 16:53:52 +0530

    deriving & outputting data based on state

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
+import { useState } from 'react'; 
+
 import { CORE_CONCEPTS } from './data.js';
 import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept.jsx';
 import TabButton from './components/TabButton.jsx';
+import { EXAMPLES } from './data.js';
 
 function App() {
+  const [ selectedTopic, setSelectedTopic ] =  useState('components')
+  // let tabContent = 'Please click a button'
+  function hendleSelect(selectedButton) {
+    // selected button => 'components', 'jsx', 'props', 'state'
+    setSelectedTopic(selectedButton);
+    console.log(selectedTopic)
+  }
   return (
     <div>
       <Header/>
@@ -24,11 +34,20 @@ function App() {
         <section id='examples'>
           <h2>Example</h2>
           <menu>
-            <TabButton>Components</TabButton>
-            <TabButton>JSX</TabButton>
-            <TabButton>Props</TabButton>
-            <TabButton>State</TabButton>
+            <TabButton onSelect={() => hendleSelect('components')}>Components</TabButton>
+            <TabButton onSelect={() => hendleSelect('jsx')}>JSX</TabButton>
+            <TabButton onSelect={() => hendleSelect('props')}>Props</TabButton>
+            <TabButton onSelect={() => hendleSelect('state')}>State</TabButton>
           </menu>
+          <div id='tab-content'>
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>
+                {EXAMPLES[selectedTopic].code}
+              </code>
+            </pre>
+          </div>
         </section>
       </main>
     </div>
--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,7 +1,8 @@
-export default function TabButton({ children }) {
+export default function TabButton({ children, onSelect }) {
+  
   return (
     <li>
-      <button>{children}</button>
+      <button onClick={onSelect}>{children}</button>
     </li>
   );
 }
